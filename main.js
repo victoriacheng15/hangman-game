96b17c0707a6117f2e867787e37103ca1ee1faad
Victoria Cheng <victoria.cheng15@gmail.com>
2022-04-09 13:44:31 -0400

    add keyboard keys in html instead of JS

--- main.js
+++ main.js
@@ -1,25 +1,5 @@
 import options from './fetch';
 
-function makeButton(array) {
-  const keyboard = document.querySelector('[data-keyboard]');
-  const div = document.createElement('div');
-  div.classList.add('row');
-  array.forEach((letter) => {
-    const btn = document.createElement('button');
-    btn.classList.add('key');
-    btn.textContent = letter;
-    div.appendChild(btn);
-  });
-  keyboard.appendChild(div);
-}
-
-const row1 = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'];
-const row2 = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
-const row3 = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
-makeButton(row1);
-makeButton(row2);
-makeButton(row3);
-
 async function fetchWrod() {
   const url = 'https://random-words-with-pronunciation.p.rapidapi.com/word';
   const data = await (await fetch(url, options)).json();
